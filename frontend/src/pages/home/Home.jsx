52030e525d54d2695f056b14452bb389d4520eba
Abhishek Kumar <abhishekkumar.06099@gmail.com>
2025-01-03 23:35:43 +0530

    Added Home Page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Home.css"
+import Testimonials from "../../components/testimonials/testimonials";
 const Home=()=>{
-    return <div> Home </div>
+    const navigate = useNavigate(); 
+    return (
+        <div className="home">
+            <div className="home-content">
+                <h1>Welcome to our E-Learning Platform</h1>
+                <p>Learn,Excel,Grow</p>
+                <button className="common-btn"  onClick={()=>navigate("/courses")}>Get Started</button>
+
+            </div>
+            <Testimonials/>
+
+        </div>
+    )
 }
 export default Home;
